d3cdef12ee0c5fe0c9790acf6c21563abb4e81b0
Carla Joseph <carlajseph@gmail.com>
2021-11-28 17:13:39 -0800

    Done

---
+++ problem1/problemOne.js
@@ -0,0 +1,12 @@
+function multiplesOf3And5(number) {
+  let total = 0;
+
+  for (i = 0; i <= number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      total += i;
+    }
+  }
+  return total;
+}
+
+multiplesOf3or5(1000);
